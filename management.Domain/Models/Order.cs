aa7872c3fbb443546441f4cdcd8640984d3073e1
JavierDevPro <gjangsam@gmail.com>
2025-10-26 17:02:55 -0500

    Merge from main changed

--- management.Domain/Models/Order.cs
+++ management.Domain/Models/Order.cs
@@ -1,6 +1,13 @@
-namespace management.Domain.Models;
-
-public class Order
+namespace management.Domain.Models
 {
-    
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Pendiente";
+
+        public Customer Customer { get; set; }
+     
+    }
 }
--- management.Infrastructure/Data/AppDbContext.cs
+++ management.Infrastructure/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace management.Infrastructure.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<Customer> customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
     
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
     public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
@@ -14,10 +15,18 @@ public class AppDbContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
+        // === RELACIONES ===
         modelBuilder.Entity<OrderDetail>().ToTable("OrderDetails");
         modelBuilder.Entity<OrderDetail>()
             .Property(o => o.UnitPrice)
             .HasColumnType("decimal(10,2)");
-    }
         
+        // Un cliente puede tener muchos pedidos
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId);
     }
+}
